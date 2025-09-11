9d7c31f259a13bc74c8423a727e599d3956cb1fc
Tom-Erik Paulsen <tepaulsen@gmail.com>
2025-09-11 10:12:09 +0200

    translations

--- resources/lang/en/translation.php
+++ resources/lang/en/translation.php
@@ -9,7 +9,7 @@ return [
     |
     | This language file contains the translations for the main menu items,
     | personal information section, form labels, countries, and buttons.
-    | Generated from database on 2025-09-10 14:27:39
+    | Generated from database on 2025-09-10 14:28:58
     |
     */
 
--- resources/lang/se/translation.php
+++ resources/lang/se/translation.php
@@ -9,9 +9,14 @@ return [
     |
     | This language file contains the translations for the main menu items,
     | personal information section, form labels, countries, and buttons.
-    | Generated from database on 2025-09-10 14:27:39
+    | Generated from database on 2025-09-10 14:28:58
     |
     */
+
+    // categories
+    'categories' => [
+        'parking' => 'parkering',
+    ],
 
     // Root-level translations
 
